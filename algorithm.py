afa938728232314490eed3f60098fcc60fb73404
jhj603 <82034869+jhj603@users.noreply.github.com>
2025-07-10 21:32:13 +0900

    feat: 10869

--- algorithm.py
+++ algorithm.py
@@ -1,1 +1,11 @@
-print('Hello World!')
+raw_input = input()
+raw_input = raw_input.split()
+
+A = int(raw_input[0])
+B = int(raw_input[1])
+
+print(A + B)
+print(A - B)
+print(A * B)
+print(A // B)
+print(A % B)
