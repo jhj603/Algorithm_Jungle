e0bd483c662c861b4c28b184d475587d9c67812e
jhj603 <82034869+jhj603@users.noreply.github.com>
2025-07-10 22:16:56 +0900

    feat: 9498

--- algorithm.py
+++ algorithm.py
@@ -1,1 +1,12 @@
-print('Hello World!')
+A = int(input())
+
+if (89 < A):
+    print('A')
+elif (79 < A):
+    print('B')
+elif (69 < A):
+    print('C')
+elif(59 < A):
+    print('D')
+else:
+    print('F')
