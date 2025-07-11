2a08904e25cc674ce4b53a9039c5506b66866d1f
jhj603 <82034869+jhj603@users.noreply.github.com>
2025-07-11 10:37:12 +0900

    feat: 2562

--- algorithm.py
+++ algorithm.py
@@ -1,1 +1,14 @@
-print('Hello World!')
+from sys import stdin
+
+Max = 0
+idx = -1
+
+for i in range(9):
+    N = int(stdin.readline().split()[0])
+
+    if (Max < N):
+        Max = N
+        idx = i
+
+print(Max)
+print(idx + 1)
