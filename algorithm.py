019493c00cb1d4f5143b2dbaa5daa4c7cf9f07d3
jhj603 <82034869+jhj603@users.noreply.github.com>
2025-07-11 20:55:37 +0900

    feat: 1978

--- algorithm.py
+++ algorithm.py
@@ -1,5 +1,26 @@
 from sys import stdin
+import math
 
 stdin = open('input.txt', 'r')
 
+def is_Prime(num):
+    if 1 == num:
+        return False
+    
+    for i in range(2, int(math.sqrt(num)) + 1):
+        if 0 == num % i:
+            return False
+        
+    return True
 
+stdin.readline()
+
+Array = list(map(int, stdin.readline().strip().split()))
+
+Count = 0
+
+for a in Array:
+    if is_Prime(a):
+        Count += 1
+
+print(Count)
