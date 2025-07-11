c413fee3855fea3f7bffd34ef65722c77bd486ef
jhj603 <82034869+jhj603@users.noreply.github.com>
2025-07-11 22:35:00 +0900

    feat: 1065

--- algorithm.py
+++ algorithm.py
@@ -2,4 +2,24 @@ from sys import stdin
 
 stdin = open('input.txt', 'r')
 
+def is_Hansu(num):
+    if 100 > num:
+        return True
+    
+    strTemp = str(num)
+    Array = list(map(int, strTemp))
 
+    if ((Array[1] - Array[0]) == (Array[2] - Array[1])):
+        return True
+    
+    return False
+
+Count = 0
+
+N = int(stdin.readline())
+
+for i in range(1, N + 1):
+    if is_Hansu(i):
+        Count += 1
+
+print(Count)
