b2eff4fa87ba0f8ec9b2d0aadd64fbc0921dba5e
jhj603 <82034869+jhj603@users.noreply.github.com>
2025-07-11 21:57:01 +0900

    feat: 9020

--- algorithm.py
+++ algorithm.py
@@ -2,4 +2,21 @@ from sys import stdin
 
 stdin = open('input.txt', 'r')
 
+Prime = [True] * 10001
 
+Prime[0] = Prime[1] = False
+
+for i in range(2, int(10001 ** 0.5) + 1):
+    if Prime[i]:
+        for j in range(i * i, 10001, i):
+            Prime[j] = False
+
+T = int(stdin.readline())
+
+for i in range(T):
+    N = int(stdin.readline())
+
+    for i in range(N // 2, 1, -1):
+        if Prime[i] and Prime[N - i]:
+            print(f"{i} {N - i}")
+            break
