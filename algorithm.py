0afb2134485c279efa913648f0052e7e8f226062
jhj603 <82034869+jhj603@users.noreply.github.com>
2025-07-11 19:38:49 +0900

    feat: 1152

--- algorithm.py
+++ algorithm.py
@@ -1,5 +1,12 @@
 from sys import stdin
 
-stdin = open('input.txt', 'r')
+#stdin = open('input.txt', 'r')
 
+Array = list(stdin.readline().strip().split(' '))
 
+Count = 0
+for Str in Array:
+    if "" != Str:
+        Count += 1
+
+print(Count)
