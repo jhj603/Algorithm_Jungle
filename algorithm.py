87455e55fa4733fe53dd1a95a3b0b1081cff332b
jhj603 <82034869+jhj603@users.noreply.github.com>
2025-07-11 19:46:25 +0900

    feat: 2908

--- algorithm.py
+++ algorithm.py
@@ -2,4 +2,12 @@ from sys import stdin
 
 stdin = open('input.txt', 'r')
 
+Array = list(stdin.readline().strip().split())
 
+A = int(Array[0][::-1])
+B = int(Array[1][::-1])
+
+if (A > B):
+    print(A)
+else:
+    print(B)
