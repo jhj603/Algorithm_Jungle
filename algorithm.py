795f409675b64d525e25fd6397f07432aa0b18e0
jhj603 <82034869+jhj603@users.noreply.github.com>
2025-07-11 13:18:47 +0900

    feat: 2675

--- algorithm.py
+++ algorithm.py
@@ -1,2 +1,16 @@
 from sys import stdin
 
+T = int(stdin.readline().strip())
+
+for i in range(T):
+    Input = list(stdin.readline().split())
+
+    R = int(Input[0])
+    S = Input[1] 
+
+    word_list = []
+    for word in S:
+        for count in range(R):
+            word_list.append(word)
+    
+    print("".join(word_list))
