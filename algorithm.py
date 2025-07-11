e280a18cc8a470a6635ec38e2658639bcc1df84b
jhj603 <82034869+jhj603@users.noreply.github.com>
2025-07-11 10:30:40 +0900

    feat: 10871

--- algorithm.py
+++ algorithm.py
@@ -1,1 +1,12 @@
-print('Hello World!')
+from sys import stdin
+
+Input = list(map(int, stdin.readline().split()))
+N = Input[0]
+X = Input[1]
+
+A = list(map(int, stdin.readline().split()))
+
+for a in A:
+    if (X > a):
+        print(a, end=" ")
+print()
