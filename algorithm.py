730a6c6a31df1b540ccfbc2ec9320d0ac77bb2ae
jhj603 <82034869+jhj603@users.noreply.github.com>
2025-07-11 20:28:25 +0900

    feat: 2869

--- algorithm.py
+++ algorithm.py
@@ -1,5 +1,11 @@
 from sys import stdin
+import math
 
 stdin = open('input.txt', 'r')
 
+A, B, V = map(int, stdin.readline().strip().split())
 
+Count = 1
+D = math.ceil((V - A) / (A - B))
+
+print(Count + D)
