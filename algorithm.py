2f02b893d9aa92c6d344a73c85bd9d56fe6d1316
jhj603 <82034869+jhj603@users.noreply.github.com>
2025-07-11 11:51:27 +0900

    feat: 2577

--- algorithm.py
+++ algorithm.py
@@ -1,2 +1,13 @@
 from sys import stdin
 
+Count = [0] * 10
+
+A = int(stdin.readline().strip())
+B = int(stdin.readline().strip())
+C = int(stdin.readline().strip())
+
+for word in str(A * B * C):
+    Count[int(word)] += 1
+
+for c in Count:
+    print(c)
