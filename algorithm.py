a9f761452af44392070b11721ae556c9192aeeb6
jhj603 <82034869+jhj603@users.noreply.github.com>
2025-07-11 11:06:20 +0900

    feat: 8958

--- algorithm.py
+++ algorithm.py
@@ -1,2 +1,16 @@
 from sys import stdin
 
+T = int(stdin.readline().split()[0])
+
+for i in range(T):
+    Count = Sum = 0
+    Input = stdin.readline()
+
+    for word in Input:
+        if 'O' == word:
+            Count += 1
+            Sum += Count
+        else:
+            Count = 0
+
+    print(Sum)
