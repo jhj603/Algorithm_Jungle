f6f23f673240bd93857bc8ed8ca193c018600e31
jhj603 <82034869+jhj603@users.noreply.github.com>
2025-07-12 20:51:34 +0900

    feat: 1181

--- algorithm.py
+++ algorithm.py
@@ -1,5 +1,15 @@
 from sys import stdin
 
-stdin = open('input.txt', 'r')
+stdin = open("input.txt", "r")
 
+n = int(stdin.readline())
 
+array = []
+
+for i in range(n):
+    array.append(stdin.readline().strip())
+
+result = sorted(list(set(array)), key=lambda x: (len(x), x))
+
+for word in result:
+    print(word)
