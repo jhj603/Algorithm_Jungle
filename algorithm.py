7424d994442f9e4d2eb655ad5a0d0159820faffd
jhj603 <82034869+jhj603@users.noreply.github.com>
2025-07-12 11:27:28 +0900

    feat: 1914

--- algorithm.py
+++ algorithm.py
@@ -1,5 +1,25 @@
 from sys import stdin
 
-stdin = open('input.txt', 'r')
+stdin = open("input.txt", "r")
+
+N = int(stdin.readline())
 
 
+def hanoi_count(num):
+    return (2**N) - 1
+
+
+def hanoi_order(num, start, end, sub):
+    if 1 == num:
+        print(f"{start} {end}")
+        return
+
+    hanoi_order(num - 1, start, sub, end)
+    print(f"{start} {end}")
+    hanoi_order(num - 1, sub, end, start)
+
+
+print(hanoi_count(N))
+
+if 21 > N:
+    hanoi_order(N, 1, 3, 2)
