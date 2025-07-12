52853ae7c9a9f76afc1138889692a6d87a9c0007
jhj603 <82034869+jhj603@users.noreply.github.com>
2025-07-12 20:27:25 +0900

    feat: 10989

--- algorithm.py
+++ algorithm.py
@@ -1,5 +1,14 @@
 from sys import stdin
 
-stdin = open('input.txt', 'r')
+stdin = open("input.txt", "r")
 
+N = int(stdin.readline())
 
+Array = [0 for _ in range(10001)]
+
+for _ in range(N):
+    Array[int(stdin.readline())] += 1
+
+for i in range(10001):
+    for _ in range(Array[i]):
+        print(i)
