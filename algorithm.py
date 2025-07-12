966b31be81979307b782957d5a0415906022bfef
jhj603 <82034869+jhj603@users.noreply.github.com>
2025-07-12 10:18:15 +0900

    feat: 10872

--- algorithm.py
+++ algorithm.py
@@ -1,5 +1,13 @@
 from sys import stdin
 
-stdin = open('input.txt', 'r')
+stdin = open("input.txt", "r")
 
 
+def Factorial(num):
+    if 1 >= num:
+        return 1
+
+    return num * Factorial(num - 1)
+
+
+print(Factorial(int(stdin.readline())))
