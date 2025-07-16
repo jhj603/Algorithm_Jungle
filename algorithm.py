b90c053c6668a1efd843dbecaf4d0ee06a8a524d
jhj603 <82034869+jhj603@users.noreply.github.com>
2025-07-16 13:36:42 +0900

    feat: 10870

--- algorithm.py
+++ algorithm.py
@@ -1,5 +1,15 @@
 from sys import stdin
 
-stdin = open('input.txt', 'r')
+stdin = open("input.txt", "r")
+
+n = int(stdin.readline())
 
 
+def fib_tail(n, first, second):
+    if n == 0:
+        return first
+
+    return fib_tail(n - 1, second, first + second)
+
+
+print(fib_tail(n, 0, 1))
