8d4bfea7c233b5241b8563c0a4cfee5ec7b06650
jhj603 <82034869+jhj603@users.noreply.github.com>
2025-07-16 11:50:03 +0900

    feat: 30242

--- algorithm.py
+++ algorithm.py
@@ -3,54 +3,40 @@ from sys import stdin
 stdin = open("input.txt", "r")
 
 n = int(stdin.readline())
-
-col = [False for _ in range(n)]
-
-ld = [False for _ in range(n * 2 - 1)]
-rd = [False for _ in range(n * 2 - 1)]
-
 q = list(map(int, stdin.readline().split()))
 
-cur_count = 0
+col = [False] * n
+
+ld = [False] * (n * 2 - 1)
+rd = [False] * (n * 2 - 1)
 
 for i in range(n):
     if 0 != q[i]:
-        col[q[i] - 1] = ld[i + q[i] - 1] = rd[(n - 1) - (i - q[i] + 1)] = True
-        cur_count += 1
-
-can_place = False
+        col_idx = q[i] - 1
+        col[col_idx] = ld[i + col_idx] = rd[(n - 1) - (i - col_idx)] = True
 
 
-def dfs(cur_idx):
-    global can_place
-
+def queen(cur_idx):
     if n == cur_idx:
-        can_place = True
+        print(*q)
+        return True
 
-        for i in q:
-            print(i, end=" ")
-        print()
+    if 0 != q[cur_idx]:
+        return queen(cur_idx + 1)
 
-        return
-    else:
-        if 0 != q[cur_idx]:
-            dfs(cur_idx + 1)
-        else:
-            for i in range(n):
-                if (
-                    not can_place
-                    and not col[i]
-                    and not ld[cur_idx + i]
-                    and not rd[(n - 1) - (cur_idx - i)]
-                ):
-                    col[i] = ld[cur_idx + i] = rd[(n - 1) - (cur_idx - i)] = True
-                    q[cur_idx] = i + 1
-                    dfs(cur_idx + 1)
-                    q[cur_idx] = 0
-                    col[i] = ld[cur_idx + i] = rd[(n - 1) - (cur_idx - i)] = False
+    for i in range(n):
+        if not col[i] and not ld[cur_idx + i] and not rd[(n - 1) - (cur_idx - i)]:
+            col[i] = ld[cur_idx + i] = rd[(n - 1) - (cur_idx - i)] = True
+            q[cur_idx] = i + 1
+
+            if queen(cur_idx + 1):
+                return True
+
+            q[cur_idx] = 0
+            col[i] = ld[cur_idx + i] = rd[(n - 1) - (cur_idx - i)] = False
+
+    return False
 
 
-dfs(0)
-
-if not can_place:
+if not queen(0):
     print(-1)
