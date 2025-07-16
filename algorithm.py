cdfbfe76e520a6a40099349d6412c22f2f9d5ca5
jhj603 <82034869+jhj603@users.noreply.github.com>
2025-07-16 11:07:12 +0900

    feat: algorithm_lecture1

--- algorithm.py
+++ algorithm.py
@@ -1,5 +1,42 @@
 from sys import stdin
 
-stdin = open('input.txt', 'r')
+stdin = open("input.txt", "r")
 
 
+def nsum(n):
+    if n == 0:
+        return 0
+
+    return n + nsum(n - 1)
+
+
+def nexponentiation(b, n):
+    if 0 == n:
+        return 1
+
+    if not n % 2:
+        return nexponentiation(b, n // 2) ** 2
+    else:
+        return b * nexponentiation(b, n - 1)
+
+
+def tail_nexpo(b, n, cur):
+    if 0 == n:
+        return cur
+
+    return tail_nexpo(b, n - 1, cur * b)
+
+
+def fib(n):
+    if 0 == n:
+        return 0
+
+    if 1 == n:
+        return 1
+
+    return fib(n - 1) + fib(n - 2)
+
+
+print(nsum(5))
+print(nexponentiation(2, 2))
+print(tail_nexpo(2, 2, 1))
