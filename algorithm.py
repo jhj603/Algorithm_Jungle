45e3f09985aebe3f6e56b8e7e8a174619b937a79
jhj603 <82034869+jhj603@users.noreply.github.com>
2025-07-16 19:34:33 +0900

    feat: 1074 again

--- algorithm.py
+++ algorithm.py
@@ -2,41 +2,41 @@ from sys import stdin
 
 stdin = open("input.txt", "r")
 
-N, r, c = map(int, stdin.readline().split())
+n, r, c = map(int, stdin.readline().split())
 
 
-# def DFS(num, x, y):
-#     if 0 == num:
-#         return 0
+def FindZ(num, x, y):
+    count = 0
 
-#     ori_x, ori_y = x, y
+    if num:
+        half = 2 ** (num - 1)
 
-#     x //= 2
-#     y //= 2
+        if r < (x + half) and c < (y + half):
+            count += FindZ(num - 1, x, y)
+        elif r < (x + half) and c >= (y + half):
+            count += (half**2) + FindZ(num - 1, x, y + half)
+        elif r >= (x + half) and c < (y + half):
+            count += 2 * (half**2) + FindZ(num - 1, x + half, y)
+        else:
+            count += 3 * (half**2) + FindZ(num - 1, x + half, y + half)
 
-#     Min = DFS(num - 1, x, y) * 4
-
-#     Min += (ori_x - (x * 2)) * 2 + (ori_y - (y * 2))
-
-
-#     return Min
-def DFS(num, x, y):
-    if num == 0:
-        return 0
-
-    half = 2 ** (num - 1)
-
-    if r < x + half and c < y + half:
-        return DFS(num - 1, x, y)
-
-    elif r < x + half and c >= y + half:
-        return half**2 + DFS(num - 1, x, y + half)
-
-    elif r >= x + half and c < y + half:
-        return 2 * (half**2) + DFS(num - 1, x + half, y)
-
-    else:
-        return 3 * (half**2) + DFS(num - 1, x + half, y + half)
+    return count
 
 
-print(DFS(N, 0, 0))
+print(FindZ(n, 0, 0))
+
+# # def DFS(num, x, y):
+# #     if 0 == num:
+# #         return 0
+
+# #     ori_x, ori_y = x, y
+
+# #     x //= 2
+# #     y //= 2
+
+# #     Min = DFS(num - 1, x, y) * 4
+
+# #     Min += (ori_x - (x * 2)) * 2 + (ori_y - (y * 2))
+
+
+# #     return Min
