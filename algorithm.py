1843e803fa7b94a2cabd2bfb396ac980161a4967
jhj603 <82034869+jhj603@users.noreply.github.com>
2025-07-16 14:38:32 +0900

    feat: 11729

--- algorithm.py
+++ algorithm.py
@@ -1,5 +1,30 @@
 from sys import stdin
 
-stdin = open('input.txt', 'r')
+stdin = open("input.txt", "r")
+
+n = int(stdin.readline())
 
 
+def hanoi(num, start, end, sub):
+    result = []
+
+    stack = [(num, start, end, sub)]
+
+    while stack:
+        cur_num, s, e, a = stack.pop()
+
+        if 1 == cur_num:
+            result.append((s, e))
+        else:
+            stack.append((cur_num - 1, a, e, s))
+            stack.append((1, s, e, a))
+            stack.append((cur_num - 1, s, a, e))
+
+    return result
+
+
+after_hanoi = hanoi(n, 1, 3, 2)
+
+print(len(after_hanoi))
+for start, end in after_hanoi:
+    print(f"{start} {end}")
