e76d4dfc88a8aaa67be998f8cc5c96eab33961e0
jhj603 <82034869+jhj603@users.noreply.github.com>
2025-07-16 23:02:32 +0900

    feat: 10819 again

--- algorithm.py
+++ algorithm.py
@@ -2,34 +2,70 @@ from sys import stdin
 
 stdin = open("input.txt", "r")
 
-N = int(stdin.readline())
+n = int(stdin.readline())
 
-inputs = list(map(int, stdin.readline().split()))
-
-visit = [False] * N
-result_list = []
-result = 0
+m = 0
+array = list(map(int, stdin.readline().split()))
+visit = [False] * len(array)
 
 
-def dfs(current_list):
-    global result
+def solve(count, list):
+    global m
 
-    if N == len(current_list):
-        current_sum = 0
-        for i in range(N - 1):
-            current_sum += abs(current_list[i] - current_list[i + 1])
-        result = max(result, current_sum)
+    if n == count:
+        sum = 0
+
+        for i in range(len(list) - 1):
+            sum += abs(list[i] - list[i + 1])
+
+        m = max(m, sum)
+
         return
 
-    for i in range(N):
+    for i in range(n):
         if not visit[i]:
             visit[i] = True
-            current_list.append(inputs[i])
-            dfs(current_list)
-            current_list.pop()
+            list.append(array[i])
+
+            solve(count + 1, list)
+
+            list.pop()
             visit[i] = False
 
 
-dfs([])
+solve(0, [])
 
-print(result)
+print(m)
+
+
+# N = int(stdin.readline())
+
+# inputs = list(map(int, stdin.readline().split()))
+
+# visit = [False] * N
+# result_list = []
+# result = 0
+
+
+# def dfs(current_list):
+#     global result
+
+#     if N == len(current_list):
+#         current_sum = 0
+#         for i in range(N - 1):
+#             current_sum += abs(current_list[i] - current_list[i + 1])
+#         result = max(result, current_sum)
+#         return
+
+#     for i in range(N):
+#         if not visit[i]:
+#             visit[i] = True
+#             current_list.append(inputs[i])
+#             dfs(current_list)
+#             current_list.pop()
+#             visit[i] = False
+
+
+# dfs([])
+
+# print(result)
