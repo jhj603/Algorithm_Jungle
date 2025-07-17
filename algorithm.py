6748a0396e51892b11f9628bdd5df6ae43b84bf4
jhj603 <82034869+jhj603@users.noreply.github.com>
2025-07-17 17:53:05 +0900

    feat: 11053 풀이 중

--- algorithm.py
+++ algorithm.py
@@ -1,5 +1,20 @@
 from sys import stdin
 
-stdin = open('input.txt', 'r')
+stdin = open("input.txt", "r")
+
+# 이분 탐색 풀이 - 이분 탐색을 이용만 해서 푸는 문제
 
 
+# DP 풀이
+# n = int(stdin.readline())
+
+# a = list(map(int, stdin.readline().split()))
+
+# DP = [1 for _ in range(n)]  # 모든 원소는 길이 1짜리의 부분 수열이 될 수 있다.
+
+# for i in range(n):
+#     for j in range(i):
+#         if a[j] < a[i]:
+#             DP[i] = max(DP[i], DP[j] + 1)
+
+# print(max(DP))
