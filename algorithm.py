05e4c66dce719967bb5902da0dafb3d4f241364c
jhj603 <82034869+jhj603@users.noreply.github.com>
2025-07-17 14:49:35 +0900

    feat: 1920

--- algorithm.py
+++ algorithm.py
@@ -1,5 +1,36 @@
 from sys import stdin
 
-stdin = open('input.txt', 'r')
+stdin = open("input.txt", "r")
 
+n = int(stdin.readline())
 
+array = list(map(int, stdin.readline().split()))
+
+array.sort()
+
+m = int(stdin.readline())
+
+find = list(map(int, stdin.readline().split()))
+
+for find_num in find:
+    left = 0
+    right = n - 1
+
+    found = False
+
+    while left <= right:
+        mid = (left + right) // 2
+
+        if array[mid] == find_num:
+            found = True
+            break
+
+        if array[mid] > find_num:
+            right = mid - 1
+        else:
+            left = mid + 1
+
+    if found:
+        print(1)
+    else:
+        print(0)
