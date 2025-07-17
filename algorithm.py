7bda3e58a7e543703736f5edb8964b1c986a6c5b
jhj603 <82034869+jhj603@users.noreply.github.com>
2025-07-17 16:51:27 +0900

    feat: 2110

--- algorithm.py
+++ algorithm.py
@@ -1,5 +1,33 @@
 from sys import stdin
 
-stdin = open('input.txt', 'r')
+stdin = open("input.txt", "r")
 
+n, c = map(int, stdin.readline().split())
 
+array = [int(stdin.readline()) for _ in range(n)]
+
+array.sort()
+
+left = 1
+right = array[-1] - array[0]
+
+result = 0
+
+while left <= right:
+    mid = (left + right) // 2
+
+    count = 1
+    current = array[0] + mid
+
+    for i in range(n):
+        if array[i] >= current:
+            count += 1
+            current = array[i] + mid
+
+    if c <= count:
+        result = max(result, mid)
+        left = mid + 1
+    else:
+        right = mid - 1
+
+print(result)
