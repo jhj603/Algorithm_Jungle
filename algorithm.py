3624914397c7855772c4ab788849d5eb92aaf2c5
jhj603 <82034869+jhj603@users.noreply.github.com>
2025-07-17 17:19:38 +0900

    feat: 2470

--- algorithm.py
+++ algorithm.py
@@ -1,5 +1,30 @@
 from sys import stdin
 
-stdin = open('input.txt', 'r')
+stdin = open("input.txt", "r")
+
+n = int(stdin.readline())
+
+array = list(map(int, stdin.readline().split()))
+
+array.sort()
+
+result = 10**8
+
+left = ans_l = ans_r = 0
+right = n - 1
+
+while left <= right:
+    temp_sum = array[left] + array[right]
+
+    if result > abs(temp_sum):
+        result = temp_sum
+        ans_l = array[left]
+        ans_r = array[right]
+
+    if 0 < temp_sum:
+        right -= 1
+    else:
+        left += 1
 
 
+print(f"{ans_l} {ans_r}")
