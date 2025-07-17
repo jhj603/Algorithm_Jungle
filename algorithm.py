07501f2bc8d97557d6c13c05bbe5d887384c3a23
jhj603 <82034869+jhj603@users.noreply.github.com>
2025-07-17 22:25:41 +0900

    feat: 11053

--- algorithm.py
+++ algorithm.py
@@ -3,7 +3,39 @@ from sys import stdin
 stdin = open("input.txt", "r")
 
 # 이분 탐색 풀이 - 이분 탐색을 이용만 해서 푸는 문제
+n = int(stdin.readline())
 
+a = list(map(int, stdin.readline().split()))
+
+lis = []
+
+
+def binary_search(list, find):
+    idx = len(list)
+
+    left = 0
+    right = len(list) - 1
+
+    while left <= right:
+        mid = (left + right) // 2
+
+        if list[mid] >= find:
+            idx = mid
+            right = mid - 1
+        else:
+            left = mid + 1
+
+    return idx
+
+
+for i in a:
+    if not lis or (lis[-1] < i):
+        lis.append(i)
+    else:
+        lis[binary_search(lis, i)] = i
+
+
+print(len(lis))
 
 # DP 풀이
 # n = int(stdin.readline())
