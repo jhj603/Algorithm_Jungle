260f5833dab8ac424104486cb780b8662e78ba07
jhj603 <82034869+jhj603@users.noreply.github.com>
2025-07-18 23:22:46 +0900

    feat: 17608

--- algorithm.py
+++ algorithm.py
@@ -1,5 +1,19 @@
 from sys import stdin
 
-stdin = open('input.txt', 'r')
+stdin = open("input.txt", "r")
 
+max_h = 0
+count = 0
+stack = []
 
+n = int(stdin.readline())
+
+for _ in range(n):
+    stack.append(int(stdin.readline()))
+
+for i in range(n - 1, -1, -1):
+    if max_h < stack[i]:
+        max_h = stack[i]
+        count += 1
+
+print(count)
