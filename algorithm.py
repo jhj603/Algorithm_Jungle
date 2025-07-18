fcfc5485c587ea7ec8243b0e4fd8311e808bb2de
jhj603 <82034869+jhj603@users.noreply.github.com>
2025-07-18 23:09:58 +0900

    feat: 9012

--- algorithm.py
+++ algorithm.py
@@ -1,5 +1,28 @@
 from sys import stdin
 
-stdin = open('input.txt', 'r')
+stdin = open("input.txt", "r")
 
+t = int(stdin.readline())
 
+res = []
+
+for _ in range(t):
+    is_vps = True
+    count = 0
+    for i in stdin.readline().strip():
+        if "(" == i:
+            count += 1
+        elif ")" == i:
+            if not count:
+                is_vps = False
+                break
+            else:
+                count -= 1
+
+    if is_vps and not count:
+        res.append("YES")
+    else:
+        res.append("NO")
+
+for i in res:
+    print(i)
