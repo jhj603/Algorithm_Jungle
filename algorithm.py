151a40bad16cb869f2dfb4c64a101e4acd75c23e
jhj603 <82034869+jhj603@users.noreply.github.com>
2025-07-18 17:53:44 +0900

    feat: 1629

--- algorithm.py
+++ algorithm.py
@@ -1,5 +1,20 @@
 from sys import stdin
 
-stdin = open('input.txt', 'r')
+stdin = open("input.txt", "r")
+
+a, b, c = map(int, stdin.readline().split())
 
 
+def multi(count):
+    if 1 == count:
+        return a % c
+
+    temp = multi(count // 2)
+
+    if count % 2:
+        return (temp * temp * (a % c)) % c
+    else:
+        return (temp * temp) % c
+
+
+print(multi(b))
