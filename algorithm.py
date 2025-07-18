7387d1a121d666bcb89224afb7405bf15d17ecd5
jhj603 <82034869+jhj603@users.noreply.github.com>
2025-07-18 22:54:11 +0900

    feat: 10773

--- algorithm.py
+++ algorithm.py
@@ -1,5 +1,17 @@
 from sys import stdin
 
-stdin = open('input.txt', 'r')
+stdin = open("input.txt", "r")
 
+stack = []
 
+k = int(stdin.readline())
+
+for _ in range(k):
+    num = int(stdin.readline())
+
+    if 0 == num:
+        stack.pop()
+    else:
+        stack.append(num)
+
+print(sum(stack))
