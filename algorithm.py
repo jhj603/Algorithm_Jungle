20ffc21539d0368d4f0b331be9269c7ccf4db3e1
jhj603 <82034869+jhj603@users.noreply.github.com>
2025-07-18 13:11:25 +0900

    feat: 2630

--- algorithm.py
+++ algorithm.py
@@ -1,5 +1,44 @@
 from sys import stdin
+import math
 
-stdin = open('input.txt', 'r')
+stdin = open("input.txt", "r")
+
+n = int(stdin.readline())
+
+paper = [list(map(int, stdin.readline().split())) for _ in range(n)]
+
+count = int(math.log(n, 2))
+
+white = blue = 0
+
+add = [(0, 0), (0, 1), (1, 0), (1, 1)]
 
 
+def cut_paper(num, x, y):
+    global white, blue
+
+    white_count = blue_count = 0
+
+    for i in range(2**num):
+        for j in range(2**num):
+            if not paper[x + i][y + j]:
+                white_count += 1
+            else:
+                blue_count += 1
+
+    if white_count:
+        if not blue_count:
+            white += 1
+        else:
+            for a in add:
+                cut_paper(
+                    num - 1, x + a[0] * (2 ** (num - 1)), y + a[1] * (2 ** (num - 1))
+                )
+    else:
+        blue += 1
+
+
+cut_paper(count, 0, 0)
+
+print(white)
+print(blue)
