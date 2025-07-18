6faee546722c270598d6eac580f96e55995b45e7
jhj603 <82034869+jhj603@users.noreply.github.com>
2025-07-18 18:59:58 +0900

    feat: 10828

--- algorithm.py
+++ algorithm.py
@@ -1,5 +1,25 @@
 from sys import stdin
 
-stdin = open('input.txt', 'r')
+stdin = open("input.txt", "r")
 
+n = int(stdin.readline())
 
+stack = []
+res = []
+
+for _ in range(n):
+    inputs = list(stdin.readline().split())
+
+    if "push" == inputs[0]:
+        stack.append(int(inputs[1]))
+    elif "pop" == inputs[0]:
+        res.append(-1 if not stack else stack.pop())
+    elif "size" == inputs[0]:
+        res.append(len(stack))
+    elif "empty" == inputs[0]:
+        res.append(1 if not stack else 0)
+    elif "top" == inputs[0]:
+        res.append(-1 if not stack else stack[-1])
+
+for i in res:
+    print(i)
