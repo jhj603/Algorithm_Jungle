7d5374735853d667a6efa68e293c94da71ed5b77
jhj603 <82034869+jhj603@users.noreply.github.com>
2025-07-21 01:52:22 +0900

    feat: 11866

--- algorithm.py
+++ algorithm.py
@@ -1,5 +1,17 @@
 from sys import stdin
+from collections import deque
 
 stdin = open('input.txt', 'r')
 
+n, k = map(int, stdin.readline().split())
 
+queue = deque(range(1, n + 1))
+
+result = []
+
+while queue:
+    queue.rotate(-(k - 1))
+
+    result.append(queue.popleft())
+
+print(f"<{', '.join(map(str, result))}>")
