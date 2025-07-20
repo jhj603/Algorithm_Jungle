752ebf795dc4fbd1e3d520ad1f15e91fd6aa5df6
jhj603 <82034869+jhj603@users.noreply.github.com>
2025-07-20 22:01:38 +0900

    feat: 2493

--- algorithm.py
+++ algorithm.py
@@ -2,4 +2,21 @@ from sys import stdin
 
 stdin = open('input.txt', 'r')
 
+n = int(stdin.readline())
+towers = list(map(int, stdin.readline().split()))
 
+res = []
+stack = []
+
+for i in range(n):
+    while stack and towers[stack[-1]] <= towers[i]:
+        stack.pop()
+
+    if not stack:
+        res.append(0)
+    else:
+        res.append(stack[-1] + 1)
+    
+    stack.append(i) 
+
+print(*res)
