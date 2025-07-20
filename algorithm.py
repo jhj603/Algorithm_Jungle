883d2e6276c0dfc488862b95ff7df33ca415942d
jhj603 <82034869+jhj603@users.noreply.github.com>
2025-07-21 01:27:08 +0900

    feat: 2164

--- algorithm.py
+++ algorithm.py
@@ -1,5 +1,18 @@
 from sys import stdin
+from collections import deque
 
 stdin = open('input.txt', 'r')
 
+n = int(stdin.readline())
 
+queue = deque()
+
+for i in range(1, n + 1):
+    queue.append(i)
+
+while 1 < len(queue):
+    queue.popleft()
+
+    queue.append(queue.popleft())
+
+print(queue[0])
