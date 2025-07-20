e4ec1707ae83741619fbcf1c0f6b42ea528b483c
jhj603 <82034869+jhj603@users.noreply.github.com>
2025-07-21 00:59:56 +0900

    feat: 10000 진행 중

--- algorithm.py
+++ algorithm.py
@@ -2,4 +2,27 @@ from sys import stdin
 
 stdin = open('input.txt', 'r')
 
+n = int(stdin.readline())
 
+circles = []
+
+stack = []
+
+def get_interval(element):
+    return element[1] - element[0]
+
+for _ in range(n):
+    x, r = map(int, stdin.readline().split())
+
+    left = x - r
+    right = x + r
+
+    circles.append((left, right))
+
+circles.sort(key=get_interval, reverse=True)
+
+for i in range(n):
+    if not stack:
+        stack.append(circles[i])
+
+a = 10
