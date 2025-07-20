f96a1d7220a5acb199b9070a3fb94598d079d3ec
jhj603 <82034869+jhj603@users.noreply.github.com>
2025-07-21 01:21:56 +0900

    feat: 18258

--- algorithm.py
+++ algorithm.py
@@ -1,5 +1,41 @@
 from sys import stdin
+from collections import deque
 
 stdin = open('input.txt', 'r')
 
+n = int(stdin.readline())
 
+queue = deque()
+
+result = []
+
+for _ in range(n):
+    inputs = list(stdin.readline().split())
+
+    if 'push' == inputs[0]:
+        queue.append(int(inputs[1]))
+    elif 'pop' == inputs[0]:
+        if not queue:
+            result.append(-1)
+        else:
+            result.append(queue.popleft())
+    elif 'size' == inputs[0]:
+        result.append(len(queue))
+    elif 'empty' == inputs[0]:
+        if queue:
+            result.append(0)
+        else:
+            result.append(1)
+    elif 'front' == inputs[0]:
+        if not queue:
+            result.append(-1)
+        else:
+            result.append(queue[0])
+    elif 'back' == inputs[0]:
+        if not queue:
+            result.append(-1)
+        else:
+            result.append(queue[-1])
+
+for i in result:
+    print(i)    
