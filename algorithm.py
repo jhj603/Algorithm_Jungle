620a21e9bcce498021013e0ac2feb7f1a188f457
jhj603 <82034869+jhj603@users.noreply.github.com>
2025-07-22 02:07:26 +0900

    feat: 1655

--- algorithm.py
+++ algorithm.py
@@ -1,5 +1,24 @@
 from sys import stdin
+import heapq
 
 stdin = open('input.txt', 'r')
 
+n = int(stdin.readline())
 
+max_que = []
+min_que = []
+
+for _ in range(n):
+    k = int(stdin.readline())
+
+    if len(max_que) == len(min_que):
+        heapq.heappush(max_que, -k)
+    else:
+        heapq.heappush(min_que, k)
+
+    if max_que and min_que and -max_que[0] > min_que[0]:
+        temp = -heapq.heappop(max_que)
+        heapq.heappush(max_que, -heapq.heappop(min_que))
+        heapq.heappush(min_que, temp)
+    
+    print(-max_que[0])
