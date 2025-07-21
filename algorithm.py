2bd80440bd9d701a5fe7c1e74aca1d3b1b644812
jhj603 <82034869+jhj603@users.noreply.github.com>
2025-07-22 02:16:28 +0900

    feat: 1715

--- algorithm.py
+++ algorithm.py
@@ -1,5 +1,22 @@
 from sys import stdin
+import heapq
 
 stdin = open('input.txt', 'r')
 
+pq = []
 
+n = int(stdin.readline())
+
+ans = 0
+
+for _ in range(n):
+    heapq.heappush(pq, int(stdin.readline()))
+
+while 1 < len(pq):
+    a = heapq.heappop(pq)
+    b = heapq.heappop(pq)
+
+    ans += (a + b)
+    heapq.heappush(pq, a + b)
+
+print(ans)
