70066df902b80d5dc0b640816fbf4ef6be550c32
jhj603 <82034869+jhj603@users.noreply.github.com>
2025-07-22 20:26:27 +0900

    feat: 2261 진행 중

--- algorithm.py
+++ algorithm.py
@@ -2,4 +2,15 @@ from sys import stdin
 
 stdin = open('input.txt', 'r')
 
+n = int(stdin.readline())
 
+dots = []
+
+for _ in range(n):
+    x, y = map(int, stdin.readline().split())
+
+    dots.append([x, y])
+
+dots.sort()
+
+a = 10
