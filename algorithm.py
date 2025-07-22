65c65556f814720eaf6d4d8e99c2f066de3cb4ba
jhj603 <82034869+jhj603@users.noreply.github.com>
2025-07-22 14:43:38 +0900

    feat: 13334

--- algorithm.py
+++ algorithm.py
@@ -1,5 +1,35 @@
 from sys import stdin
+import heapq
 
 stdin = open('input.txt', 'r')
 
+n = int(stdin.readline())
 
+lines = []
+pq = []
+
+max_count = 0
+
+for _ in range(n):
+    x, y = map(int, stdin.readline().split())
+    
+    lines.append((min(x, y), max(x, y)))
+
+lines.sort(key=lambda line: (line[1], line[0]))
+
+d = int(stdin.readline())
+
+for i in lines:
+    start = i[1] - d
+
+    if start > i[0]:
+        continue
+        
+    heapq.heappush(pq, i)
+
+    while pq and pq[0][0] < start:
+        heapq.heappop(pq)
+
+    max_count = max(max_count, len(pq))
+
+print(max_count)
