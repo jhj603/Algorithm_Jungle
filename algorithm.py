f1775dc7d95d01611886e269e4aa8bfb541c0421
jhj603 <82034869+jhj603@users.noreply.github.com>
2025-07-22 21:09:20 +0900

    feat: 2812

--- algorithm.py
+++ algorithm.py
@@ -1,5 +1,25 @@
 from sys import stdin
+from collections import deque
 
 stdin = open('input.txt', 'r')
 
+stack = deque()
 
+n, k = map(int, stdin.readline().split())
+
+cur_count = 0
+
+for i in stdin.readline().strip():
+    while stack and (cur_count < k) and (stack[-1] < i):
+        stack.pop()
+        cur_count += 1
+
+    stack.append(i)
+
+while cur_count < k:
+    stack.pop()
+    cur_count += 1
+
+result = "".join(stack)
+
+print(result)
