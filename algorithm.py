3eb02ba95fdfbbd558a9488609944ffd4a492b97
jhj603 <82034869+jhj603@users.noreply.github.com>
2025-07-23 11:40:11 +0900

    feat: 2504 연동

--- algorithm.py
+++ algorithm.py
@@ -1,6 +1,6 @@
 from sys import stdin
 
-stdin = open('input.txt', 'r')
+stdin = open("input.txt", "r")
 
 inputs = stdin.readline()
 
@@ -11,52 +11,52 @@ sum = 0
 wrong = False
 
 for i in inputs:
-    if '(' == i or '[' == i:
+    if "(" == i or "[" == i:
         stack.append(i)
-    elif ')' == i:
-        if not stack or '[' == stack[-1]:
+    elif ")" == i:
+        if not stack or "[" == stack[-1]:
             wrong = True
             break
 
         temp = 0
-        while stack and '(' != stack[-1]:
+        while stack and "(" != stack[-1]:
             if not isinstance(stack[-1], int):
                 wrong = True
                 break
             temp += stack.pop()
-            
+
         if wrong:
             break
-        
-        if not stack or '(' != stack[-1]:
+
+        if not stack or "(" != stack[-1]:
             wrong = True
             break
 
         stack.pop()
         stack.append(max(temp * 2, 2))
-        
-    elif ']' == i:
-        if not stack or '(' == stack[-1]:
+
+    elif "]" == i:
+        if not stack or "(" == stack[-1]:
             wrong = True
             break
 
         temp = 0
-        while stack and '[' != stack[-1]:
+        while stack and "[" != stack[-1]:
             if not isinstance(stack[-1], int):
                 wrong = True
                 break
             temp += stack.pop()
-        
+
         if wrong:
             break
 
-        if not stack or '[' != stack[-1]:
+        if not stack or "[" != stack[-1]:
             wrong = True
             break
-        
+
         stack.pop()
         stack.append(max(temp * 3, 3))
-        
+
 for i in stack:
     if not isinstance(i, int):
         wrong = True
