39e551049dc6ff3d3492bc467d1965beaf6b902e
jhj603 <82034869+jhj603@users.noreply.github.com>
2025-07-23 15:54:19 +0900

    feat: 10000

--- algorithm.py
+++ algorithm.py
@@ -1,28 +1,50 @@
 from sys import stdin
+import heapq
 
-stdin = open('input.txt', 'r')
+stdin = open("input.txt", "r")
 
 n = int(stdin.readline())
 
-circles = []
-
-stack = []
-
-def get_interval(element):
-    return element[1] - element[0]
+end_diameter = []
 
 for _ in range(n):
     x, r = map(int, stdin.readline().split())
 
-    left = x - r
     right = x + r
 
-    circles.append((left, right))
+    end_diameter.append((right, r * 2))
 
-circles.sort(key=get_interval, reverse=True)
+end_diameter.sort()
+
+pq = []
+count = 1
 
 for i in range(n):
-    if not stack:
-        stack.append(circles[i])
+    cur_end, cur_dia = end_diameter[i]
+    cur_start = cur_end - cur_dia
 
-a = 10
+    can_fill = False
+    fill_point = cur_end
+
+    while pq:
+        e, d = heapq.heappop(pq)
+        e = -e
+
+        if e <= cur_start:
+            heapq.heappush(pq, (-e, d))
+            break
+        if e != fill_point and cur_start <= (e - d):
+            continue
+        if cur_start <= (e - d):
+            fill_point = e - d
+        if fill_point == cur_start:
+            can_fill = True
+
+    count += 1
+
+    if can_fill:
+        count += 1
+
+    heapq.heappush(pq, (-cur_end, cur_dia))
+
+print(count)
