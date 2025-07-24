80c88ad8652b8fb84ac3e504aa6830003752ccca
jhj603 <82034869+jhj603@users.noreply.github.com>
2025-07-24 17:45:25 +0900

    feat: 1260

--- algorithm.py
+++ algorithm.py
@@ -1,5 +1,54 @@
 from sys import stdin
+from collections import deque
 
-stdin = open('input.txt', 'r')
+stdin = open("input.txt", "r")
+
+n, m, v = map(int, stdin.readline().split())
+
+board = [[] for _ in range(n + 1)]
+visit = [False for _ in range(n + 1)]
+
+for _ in range(m):
+    x, y = map(int, stdin.readline().split())
+
+    board[x].append(y)
+    board[y].append(x)
+
+for i in range(1, n + 1):
+    board[i].sort()
 
 
+def DFS(idx):
+    print(idx, end=" ")
+
+    for i in board[idx]:
+        if not visit[i]:
+            visit[i] = True
+            DFS(i)
+
+
+def BFS(idx):
+    que = deque()
+
+    visit[idx] = True
+    que.append(idx)
+
+    while que:
+        current = que.popleft()
+
+        print(current, end=" ")
+
+        for i in board[current]:
+            if not visit[i]:
+                visit[i] = True
+                que.append(i)
+
+
+visit[v] = True
+DFS(v)
+print()
+
+visit = [False for _ in range(n + 1)]
+
+BFS(v)
+print()
