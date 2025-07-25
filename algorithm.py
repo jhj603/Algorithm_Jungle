108307d9b57d55175e14a0ef565b84a78f9aaa9d
jhj603 <82034869+jhj603@users.noreply.github.com>
2025-07-25 14:09:40 +0900

    feat: 21606

--- algorithm.py
+++ algorithm.py
@@ -1,5 +1,59 @@
-from sys import stdin
+import sys
 
-stdin = open('input.txt', 'r')
+sys.setrecursionlimit(10**6)
+sys.stdin = open("input.txt", "r")
+
+n = int(sys.stdin.readline())
+
+tree = [[] for _ in range(n)]
+visit = [False] * n
+
+a = sys.stdin.readline().strip()
+
+for i in range(1, n):
+    u, v = map(int, sys.stdin.readline().split())
+
+    u -= 1
+    v -= 1
+
+    tree[u].append(v)
+    tree[v].append(u)
 
 
+def find_pre(start):
+    total_count = 0
+
+    for i in tree[start]:
+        if "1" == a[i]:
+            total_count += 1
+        elif not visit[i] and ("0" == a[i]):
+            visit[i] = True
+            total_count += find_pre(i)
+
+    return total_count
+
+
+def in_to_in(start):
+    can_go = 0
+
+    for i in tree[start]:
+        if "1" == a[i]:
+            can_go += 1
+
+    return can_go
+
+
+count = 0
+temp = 0
+for i in range(n):
+    if "0" == a[i]:
+        if not visit[i]:
+            visit[i] = True
+            temp = find_pre(i)
+            count += temp * (temp - 1)
+    else:
+        for node in tree[i]:
+            if "1" == a[node]:
+                count += 1
+
+print(count)
