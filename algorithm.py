7596720ec0402b6f73879d92a2e4b22619a7af99
jhj603 <82034869+jhj603@users.noreply.github.com>
2025-07-25 10:01:23 +0900

    feat: 11725

--- algorithm.py
+++ algorithm.py
@@ -1,5 +1,39 @@
 from sys import stdin
+from collections import deque
 
-stdin = open('input.txt', 'r')
+stdin = open("input.txt", "r")
+
+n = int(stdin.readline())
+
+tree = [[] for _ in range(n)]
+visit = [False] * n
+parents = [0] * n
+
+for i in range(1, n):
+    u, v = map(int, stdin.readline().split())
+
+    u -= 1
+    v -= 1
+
+    tree[u].append(v)
+    tree[v].append(u)
 
 
+def bfs(start):
+    que = deque([start])
+    visit[start] = True
+
+    while que:
+        current = que.popleft()
+
+        for i in tree[current]:
+            if not visit[i]:
+                visit[i] = True
+                parents[i] = current + 1
+                que.append(i)
+
+
+bfs(0)
+
+for i in range(1, n):
+    print(parents[i])
