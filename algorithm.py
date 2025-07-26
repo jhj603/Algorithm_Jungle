9e7765df9b864aa732d15ae0d4daf6c37782c750
jhj603 <82034869+jhj603@users.noreply.github.com>
2025-07-26 15:25:56 +0900

    feat: 2178

--- algorithm.py
+++ algorithm.py
@@ -1,5 +1,39 @@
 from sys import stdin
+from collections import deque
 
-stdin = open('input.txt', 'r')
+stdin = open("input.txt", "r")
 
+n, m = map(int, stdin.readline().split())
 
+board = []
+visit = [[-1 for _ in range(m)] for _ in range(n)]
+
+add = [(0, 1), (0, -1), (-1, 0), (1, 0)]
+
+for _ in range(n):
+    board.append(list(map(int, stdin.readline().strip())))
+
+que = deque()
+
+visit[0][0] = 1
+que.append((0, 0))
+
+while que:
+    x, y = que.popleft()
+
+    for i in range(4):
+        tempx = x + add[i][0]
+        tempy = y + add[i][1]
+
+        if (
+            (0 <= tempx)
+            and (0 <= tempy)
+            and (n > tempx)
+            and (m > tempy)
+            and board[tempx][tempy]
+            and (-1 == visit[tempx][tempy])
+        ):
+            visit[tempx][tempy] = visit[x][y] + 1
+            que.append((tempx, tempy))
+
+print(visit[n - 1][m - 1])
