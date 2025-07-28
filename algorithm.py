272f5e7a6fecc9a5475fe4384b6bf38a96c3e1e2
jhj603 <82034869+jhj603@users.noreply.github.com>
2025-07-28 15:01:44 +0900

    feat: 프림 풀이 추가

--- algorithm.py
+++ algorithm.py
@@ -1,44 +1,84 @@
 import sys
+import heapq
 
 sys.stdin = open("input.txt", "r")
 
 v, e = map(int, sys.stdin.readline().split())
 
-node_union = [i for i in range(v)]
-depth = [0] * v
+# 프림 풀이
+edges = [[] for _ in range(v)]
+visit = [False] * v
 
-line_list = []
 for _ in range(e):
     a, b, c = map(int, sys.stdin.readline().split())
-    line_list.append([c, a - 1, b - 1])
 
-line_list.sort()
+    a -= 1
+    b -= 1
 
-result = 0
+    edges[a].append((b, c))
+    edges[b].append((a, c))
+
+pq = []
+heapq.heappush(pq, (0, 0))
+
+weight = 0
+chosen_edges = 0
+
+while pq:
+    cost, cur = heapq.heappop(pq)
+
+    if visit[cur]:
+        continue
+
+    visit[cur] = True
+    weight += cost
+    chosen_edges += 1
+
+    if v == chosen_edges:
+        break
+
+    for i in edges[cur]:
+        if not visit[i[0]]:
+            heapq.heappush(pq, (i[1], i[0]))
+
+print(weight)
+
+# 크루스칼 풀이
+# node_union = [i for i in range(v)]
+# depth = [0] * v
+
+# line_list = []
+# for _ in range(e):
+#     a, b, c = map(int, sys.stdin.readline().split())
+#     line_list.append([c, a - 1, b - 1])
+
+# line_list.sort()
+
+# result = 0
 
 
-def find_parent(idx):
-    if idx == node_union[idx]:
-        return idx
+# def find_parent(idx):
+#     if idx == node_union[idx]:
+#         return idx
 
-    node_union[idx] = find_parent(node_union[idx])
+#     node_union[idx] = find_parent(node_union[idx])
 
-    return node_union[idx]
+#     return node_union[idx]
 
 
-for line in line_list:
-    a = find_parent(line[1])
-    b = find_parent(line[2])
+# for line in line_list:
+#     a = find_parent(line[1])
+#     b = find_parent(line[2])
 
-    if a != b:
-        if depth[a] < depth[b]:
-            node_union[a] = b
-        elif depth[a] > depth[b]:
-            node_union[b] = a
-        else:
-            node_union[b] = a
-            depth[a] += 1
+#     if a != b:
+#         if depth[a] < depth[b]:
+#             node_union[a] = b
+#         elif depth[a] > depth[b]:
+#             node_union[b] = a
+#         else:
+#             node_union[b] = a
+#             depth[a] += 1
 
-        result += line[0]
+#         result += line[0]
 
-print(result)
+# print(result)
