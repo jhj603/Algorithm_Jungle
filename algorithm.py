fef1643e3603b30f32af5b7cfba062ae90c42580
jhj603 <82034869+jhj603@users.noreply.github.com>
2025-07-31 21:26:39 +0900

    feat: 12865

--- algorithm.py
+++ algorithm.py
@@ -1,5 +1,15 @@
 from sys import stdin
 
-stdin = open('input.txt', 'r')
+stdin = open("input.txt", "r")
 
+n, k = map(int, stdin.readline().split())
 
+DP = [0 for _ in range(k + 1)]
+
+for _ in range(n):
+    w, v = map(int, stdin.readline().split())
+
+    for i in range(k, w - 1, -1):
+        DP[i] = max(DP[i], DP[i - w] + v)
+
+print(DP[k])
