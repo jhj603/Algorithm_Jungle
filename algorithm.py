f9209165a5574fd8a45f45fc95fc32c5dc36b361
jhj603 <82034869+jhj603@users.noreply.github.com>
2025-07-31 20:56:29 +0900

    feat: 9251

--- algorithm.py
+++ algorithm.py
@@ -1,5 +1,20 @@
 from sys import stdin
 
-stdin = open('input.txt', 'r')
+stdin = open("input.txt", "r")
 
+first = stdin.readline().strip()
+second = stdin.readline().strip()
 
+first_size = len(first)
+second_size = len(second)
+
+DP = [[0 for _ in range(first_size + 1)] for _ in range(second_size + 1)]
+
+for i in range(1, second_size + 1):
+    for j in range(1, first_size + 1):
+        if first[j - 1] == second[i - 1]:
+            DP[i][j] = DP[i - 1][j - 1] + 1
+        else:
+            DP[i][j] = max(DP[i - 1][j], DP[i][j - 1])
+
+print(DP[second_size][first_size])
