47ac525a37f7df62517e8fbf9ba8141f5bdd50d2
jhj603 <82034869+jhj603@users.noreply.github.com>
2025-07-31 20:08:56 +0900

    feat: 9084

--- algorithm.py
+++ algorithm.py
@@ -1,5 +1,22 @@
 from sys import stdin
 
-stdin = open('input.txt', 'r')
+stdin = open("input.txt", "r")
 
+T = int(stdin.readline())
 
+for _ in range(T):
+    n = int(stdin.readline())
+
+    coins = list(map(int, stdin.readline().split()))
+
+    m = int(stdin.readline())
+
+    DP = [0 for _ in range(m + 1)]
+
+    DP[0] = 1
+
+    for i in coins:
+        for j in range(i, m + 1):
+            DP[j] += DP[j - i]
+
+    print(DP[m])
