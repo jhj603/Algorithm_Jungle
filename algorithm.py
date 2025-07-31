d9d04e699f72a37903e4d41b5ffa6920c97027d9
jhj603 <82034869+jhj603@users.noreply.github.com>
2025-07-31 16:31:28 +0900

    feat: 2748

--- algorithm.py
+++ algorithm.py
@@ -1,5 +1,13 @@
 from sys import stdin
 
-stdin = open('input.txt', 'r')
+stdin = open("input.txt", "r")
 
+n = int(stdin.readline())
 
+a = 0
+b = 1
+
+for _ in range(2, n + 1):
+    a, b = b, a + b
+
+print(b)
