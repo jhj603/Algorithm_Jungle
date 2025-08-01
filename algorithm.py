a927c195b35d89391a1a0db2bbd5853e4311df30
jhj603 <82034869+jhj603@users.noreply.github.com>
2025-08-01 17:21:42 +0900

    feat: 9251 재풀이

--- algorithm.py
+++ algorithm.py
@@ -5,16 +5,16 @@ stdin = open("input.txt", "r")
 first = stdin.readline().strip()
 second = stdin.readline().strip()
 
-first_size = len(first)
-second_size = len(second)
+first_len = len(first)
+second_len = len(second)
 
-DP = [[0 for _ in range(first_size + 1)] for _ in range(second_size + 1)]
+DP = [[0 for _ in range(second_len + 1)] for _ in range(first_len + 1)]
 
-for i in range(1, second_size + 1):
-    for j in range(1, first_size + 1):
-        if first[j - 1] == second[i - 1]:
+for i in range(1, first_len + 1):
+    for j in range(1, second_len + 1):
+        if first[i - 1] == second[j - 1]:
             DP[i][j] = DP[i - 1][j - 1] + 1
         else:
             DP[i][j] = max(DP[i - 1][j], DP[i][j - 1])
 
-print(DP[second_size][first_size])
+print(DP[first_len][second_len])
