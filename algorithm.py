87a41be6493d823c783b02824d96110b7ac06eb1
jhj603 <82034869+jhj603@users.noreply.github.com>
2025-08-02 17:34:21 +0900

    feat: 2098

--- algorithm.py
+++ algorithm.py
@@ -1,5 +1,32 @@
 from sys import stdin
 
-stdin = open('input.txt', 'r')
+stdin = open("input.txt", "r")
 
+n = int(stdin.readline())
 
+board = [list(map(int, stdin.readline().split())) for _ in range(n)]
+DP = [[float("inf") for _ in range(2**n)] for _ in range(n)]
+
+DP[0][1] = 0
+
+for mask in range(1, 1 << n):
+    for cur in range(n):
+        if not mask & (1 << cur):
+            continue
+
+        for pre in range(n):
+            if not mask & (1 << pre) or pre == cur:
+                continue
+
+            pre_mask = mask ^ (1 << cur)
+
+            if board[pre][cur]:
+                DP[cur][mask] = min(DP[cur][mask], DP[pre][pre_mask] + board[pre][cur])
+
+result = float("inf")
+
+for i in range(1, n):
+    if board[i][0] and float("inf") != DP[i][(1 << n) - 1]:
+        result = min(result, DP[i][(1 << n) - 1] + board[i][0])
+
+print(result)
