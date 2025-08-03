2ae9efb766633d4e2cfa18d42f753f7c564b8834
jhj603 <82034869+jhj603@users.noreply.github.com>
2025-08-03 11:32:31 +0900

    feat: 11047

--- algorithm.py
+++ algorithm.py
@@ -1,5 +1,18 @@
 from sys import stdin
 
-stdin = open('input.txt', 'r')
+stdin = open("input.txt", "r")
 
+count = 0
 
+n, k = map(int, stdin.readline().split())
+
+a = [int(stdin.readline()) for _ in range(n)]
+
+for i in range(n - 1, -1, -1):
+    if a[i] > k:
+        continue
+
+    count += k // a[i]
+    k %= a[i]
+
+print(count)
