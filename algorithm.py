77888bf19c7af5fdddae22d802cde5df827a9b41
jhj603 <82034869+jhj603@users.noreply.github.com>
2025-08-03 17:24:12 +0900

    feat: 1946

--- algorithm.py
+++ algorithm.py
@@ -1,5 +1,24 @@
 from sys import stdin
 
-stdin = open('input.txt', 'r')
+stdin = open("input.txt", "r")
 
+t = int(stdin.readline())
 
+for _ in range(t):
+    n = int(stdin.readline())
+
+    test_result = [tuple(map(int, stdin.readline().split())) for _ in range(n)]
+
+    test_result.sort()
+
+    count = 1
+    last = test_result[0][1]
+
+    for i in range(1, n):
+        if test_result[i][1] > last:
+            continue
+
+        count += 1
+        last = test_result[i][1]
+
+    print(count)
