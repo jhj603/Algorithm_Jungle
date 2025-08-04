7c020d73019cd4588c3c6d0f1d84423e6dbfd03c
jhj603 <82034869+jhj603@users.noreply.github.com>
2025-08-04 13:14:49 +0900

    feat: 1700

--- algorithm.py
+++ algorithm.py
@@ -1,5 +1,43 @@
 from sys import stdin
+from collections import deque
 
-stdin = open('input.txt', 'r')
+stdin = open("input.txt", "r")
 
+n, k = map(int, stdin.readline().split())
 
+order = list(map(int, stdin.readline().split()))
+
+indices = [deque() for _ in range(k + 1)]
+
+for i in range(k):
+    indices[order[i]].append(i)
+
+unplug_count = 0
+
+plug = set()
+
+for i in range(k):
+    if n <= len(plug) and not order[i] in plug:
+        max_count = 0
+        max_idx = -1
+
+        for item in plug:
+            if not indices[item]:
+                gap = float("inf")
+            else:
+                gap = indices[item][0] - i
+
+            if max_count < gap:
+                max_count = gap
+                max_idx = item
+
+        plug.discard(max_idx)
+
+        unplug_count += 1
+
+    plug.add(order[i])
+
+    if indices[order[i]]:
+        indices[order[i]].popleft()
+
+print(unplug_count)
